e36b71c827e7397e04b8483138b18f2c631ffef9
mingjian.tang <mingjian.tang@rokid.com>
2023-10-20 15:49:13 +0800

    feat: 修改列表页码设置

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -14,22 +14,17 @@ const UserLayout: React.FC<UserLayoutProps> = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const { getUserInfo } = useModelDispatchers('user');
-  const { requestParams } = useModelState('list')
+  const { requestParams, defaultRequestParams } = useModelState('list')
   const { updateRequestParams } = useModelDispatchers('list')
 
   useEffect(() => {
-    // 默认行个数
-    let sizeNum: number = 4
     if (document.body.scrollWidth > 1440) {
-      // 大分辨率行个数
-      sizeNum = 6
+      /** 大分辨率增大页码，避免无法滚动 */
+      updateRequestParams({
+        ...requestParams,
+        pageSize: defaultRequestParams.pageSize * 2,
+      })
     }
-    // 根据窗口高度确定列个数
-    const lineNum = Math.round(document.body.scrollHeight / 280)
-    updateRequestParams({
-      ...requestParams,
-      pageSize: sizeNum * lineNum + 2,
-    })
   }, [])
 
   useEffect(
